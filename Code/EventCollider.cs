0c31506d0f2901c8f4af7c6015adeceada6ae872
Luke <luke@spierek.net>
2018-05-10 12:36:45 +0200

    EventColliders

---
+++ Code/EventCollider.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+namespace LSTools
+{
+    public class EventCollider : MonoBehaviour
+    {
+        [SerializeField]
+        private Collider m_Collider;
+        public Collider Collider { get { return m_Collider; } }
+
+        public AEvent<Collision> OnColliderIntersection = new AEvent<Collision>();   // entered + stayed
+        public AEvent<Collision> OnColliderEntered = new AEvent<Collision>();
+        public AEvent<Collision> OnColliderStayed = new AEvent<Collision>();
+        public AEvent<Collision> OnColliderExited = new AEvent<Collision>();
+
+        public AEvent<Collider> OnTriggerIntersection = new AEvent<Collider>();      // entered + stayed
+        public AEvent<Collider> OnTriggerEntered = new AEvent<Collider>();
+        public AEvent<Collider> OnTriggerStayed = new AEvent<Collider>();
+        public AEvent<Collider> OnTriggerExited = new AEvent<Collider>();
+
+        private void Awake()
+        {
+            GetReferences();
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            OnColliderIntersection.Invoke(collision);
+            OnColliderEntered.Invoke(collision);
+        }
+
+        private void OnCollisionStay(Collision collision)
+        {
+            OnColliderIntersection.Invoke(collision);
+            OnColliderStayed.Invoke(collision);
+        }
+
+        private void OnCollisionExit(Collision collision)
+        {
+            OnColliderExited.Invoke(collision);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            OnTriggerIntersection.Invoke(other);
+            OnTriggerEntered.Invoke(other);
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            OnTriggerIntersection.Invoke(other);
+            OnTriggerStayed.Invoke(other);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            OnTriggerExited.Invoke(other);
+        }
+
+        private void OnValidate()
+        {
+            GetReferences();
+        }
+
+        private void GetReferences()
+        {
+            if (m_Collider == null)
+            {
+                m_Collider = GetComponent<Collider>();
+            }
+        }
+    }
+
+}
---
+++ Code/EventCollider2D.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+namespace LSTools
+{
+    public class EventCollider2D : MonoBehaviour
+    {
+        [SerializeField]
+        private Collider2D m_Collider;
+        public Collider2D Collider { get { return m_Collider; } }
+
+        public AEvent<Collision2D> OnColliderIntersection = new AEvent<Collision2D>();   // entered + stayed
+        public AEvent<Collision2D> OnColliderEntered = new AEvent<Collision2D>();
+        public AEvent<Collision2D> OnColliderStayed = new AEvent<Collision2D>();
+        public AEvent<Collision2D> OnColliderExited = new AEvent<Collision2D>();
+
+        public AEvent<Collider2D> OnTriggerIntersection = new AEvent<Collider2D>();      // entered + stayed
+        public AEvent<Collider2D> OnTriggerEntered = new AEvent<Collider2D>();
+        public AEvent<Collider2D> OnTriggerStayed = new AEvent<Collider2D>();
+        public AEvent<Collider2D> OnTriggerExited = new AEvent<Collider2D>();
+
+        private void Awake()
+        {
+            GetReferences();
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            OnColliderIntersection.Invoke(collision);
+            OnColliderEntered.Invoke(collision);
+        }
+
+        private void OnCollisionStay2D(Collision2D collision)
+        {
+            OnColliderIntersection.Invoke(collision);
+            OnColliderStayed.Invoke(collision);
+        }
+
+        private void OnCollisionExit2D(Collision2D collision)
+        {
+            OnColliderExited.Invoke(collision);
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            OnTriggerIntersection.Invoke(other);
+            OnTriggerEntered.Invoke(other);
+        }
+
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            OnTriggerIntersection.Invoke(other);
+            OnTriggerStayed.Invoke(other);
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            OnTriggerExited.Invoke(other);
+        }
+
+        private void OnValidate()
+        {
+            GetReferences();
+        }
+
+        private void GetReferences()
+        {
+            if (m_Collider == null)
+            {
+                m_Collider = GetComponent<Collider2D>();
+            }
+        }
+    }
+
+}
