7dce423a4c41e24b05becc31e0e41b52fcc1b9d1
Luke <luke@spierek.net>
2018-06-17 15:27:41 +0200

    AFSMModule + state visualization

---
+++ Code/AFSMModule.cs
@@ -0,0 +1,8 @@
+﻿namespace LSTools
+{
+    // used in conjunction with an editor class to visualize current FSM state
+    public abstract class AFSMModule : AModule
+    {
+        public FiniteStateMachine FSM { get; protected set; }
+    }
+}
---
+++ Code/Editor/AFSMModuleEditor.cs
@@ -0,0 +1,65 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace LSTools
+{
+    [CustomEditor(typeof(AFSMModule), true)]
+    public class AFSMModuleEditor : Editor
+    {
+        private AFSMModule m_Module;
+
+        public override bool RequiresConstantRepaint()
+        {
+            return true;
+        }
+
+        private void OnEnable()
+        {
+            m_Module = target as AFSMModule;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            DrawFSM();
+        }
+
+        private void DrawFSM()
+        {
+            if (!Application.isPlaying || m_Module == null || m_Module.FSM == null)
+            {
+                EditorGUILayout.HelpBox("State Machine can only be previewed in Play Mode.", MessageType.Info);
+            }
+            else
+            {
+                EditorGUILayout.LabelField("State Machine:", EditorStyles.boldLabel);
+                DrawStateMachine(m_Module.FSM);
+            }
+        }
+
+        private void DrawStateMachine(FiniteStateMachine stateMachine)
+        {
+            foreach (AState state in stateMachine.GetStates().Values)
+            {
+                bool isCurrent = (stateMachine.CurrentStateId == state.Id);
+                DrawState(state, isCurrent);
+            }
+        }
+
+        private void DrawState(AState state, bool isCurrent)
+        {
+            // draw state name
+            string name = (isCurrent ? "▶" : "") + state.ToString();
+            EditorGUILayout.LabelField(name);
+
+            // draw substates
+            if (state.FSM.HasStates())
+            {
+                EditorGUI.indentLevel++;
+                DrawStateMachine(state.FSM);
+                EditorGUI.indentLevel--;
+            }
+        }
+    }
+
+}
