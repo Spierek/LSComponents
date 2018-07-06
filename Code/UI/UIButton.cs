a976c942ade5535eeaccccf1e4dffa3557396127
Luke <luke@spierek.net>
2018-07-06 12:39:04 +0200

    UIButton

---
+++ Code/UI/UIButton.cs
@@ -0,0 +1,12 @@
+﻿namespace LSTools
+{
+    public class UIButton : AUIButton
+    {
+        public readonly AEvent OnClicked = new AEvent();
+
+        protected override void HandleButtonClicked()
+        {
+            OnClicked.Invoke();
+        }
+    }
+}
