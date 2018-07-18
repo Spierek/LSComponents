6e41afa457fbc2a7b0aa20ea6830b7fb3957817b
Luke <luke@spierek.net>
2018-07-18 12:29:00 +0200

    Moved LSAssetExt to LSTools namespace

--- Code/Extensions/LSAssetExt.cs
+++ Code/Extensions/LSAssetExt.cs
@@ -3,23 +3,26 @@ using System.Collections.Generic;
 using UnityEditor;
 
 #if UNITY_EDITOR
-public class LSAssetExt : MonoBehaviour
+namespace LSTools
 {
-    // by @glitchers via http://answers.unity.com/answers/1216386/view.html
-    public static List<T> FindAssetsByType<T>() where T : Object
+    public class LSAssetExt : MonoBehaviour
     {
-        List<T> assets = new List<T>();
-        string[] guids = AssetDatabase.FindAssets(string.Format("t:{0}", typeof(T)));
-        for (int i = 0; i < guids.Length; i++)
+        // by @glitchers via http://answers.unity.com/answers/1216386/view.html
+        public static List<T> FindAssetsByType<T>() where T : Object
         {
-            string assetPath = AssetDatabase.GUIDToAssetPath( guids[i] );
-            T asset = AssetDatabase.LoadAssetAtPath<T>( assetPath );
-            if (asset != null)
+            List<T> assets = new List<T>();
+            string[] guids = AssetDatabase.FindAssets(string.Format("t:{0}", typeof(T)));
+            for (int i = 0; i < guids.Length; i++)
             {
-                assets.Add(asset);
+                string assetPath = AssetDatabase.GUIDToAssetPath( guids[i] );
+                T asset = AssetDatabase.LoadAssetAtPath<T>( assetPath );
+                if (asset != null)
+                {
+                    assets.Add(asset);
+                }
             }
+            return assets;
         }
-        return assets;
     }
 }
 #endif
