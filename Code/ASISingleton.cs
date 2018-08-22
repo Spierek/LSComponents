67ae67b1c7957ddc5b4f3db06f445c5ce9e847ad
Luke <luke@spierek.net>
2018-08-22 10:32:47 +0200

    Self instantiating singleton

---
+++ Code/ASISingleton.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace LSTools
+{
+    // self instantiating singleton
+    public abstract class ASISingleton<T> : CachedMonoBehaviour
+        where T : ASISingleton<T>
+    {
+        private static T m_Instance;
+        public static T Instance
+        {
+            get
+            {
+                if (m_Instance == null)
+                {
+                    T inst = new GameObject(string.Format("[{0}]", typeof(T))).AddComponent<T>();
+                    m_Instance = inst;
+                }
+
+                return m_Instance;
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            if (m_Instance == null)
+            {
+                m_Instance = this as T;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
