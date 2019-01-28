00faeef0c0d40586078f9753dc0686b31b1e9f25
Luke <luke@spierek.net>
2019-01-29 00:30:21 +0100

    UI animation widgets

---
+++ Code/UI/Animation/AUIAnimWidget.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace LSTools
+{
+    public abstract class AUIAnimWidget : AUIBehaviour
+    {
+        [SerializeField, Range(0.5f, 7f)]
+        protected float m_AnimationDuration = 2f;
+
+        protected float m_AnimationStartTime = 0f;
+
+        protected override void Update()
+        {
+            if (IsVisible)
+            {
+                UpdateAnimation();
+            }
+        }
+
+        protected override void HandleShow()
+        {
+            base.HandleShow();
+            m_AnimationStartTime = Time.time;
+        }
+
+        private void UpdateAnimation()
+        {
+            float totalTime = Time.time - m_AnimationStartTime;
+            float timer = totalTime % m_AnimationDuration;
+            float ratio = timer / m_AnimationDuration;
+
+            HandleAnimation(ratio);
+        }
+
+        protected abstract void HandleAnimation(float ratio);
+    }
+}
---
+++ Code/UI/Animation/UIFadeAnimWidget.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace LSTools
+{
+    public class UIFadeAnimWidget : AUIAnimWidget
+    {
+        [Space]
+        [SerializeField]
+        protected CanvasGroup m_AnimGroup;
+        [SerializeField]
+        private AnimationCurve m_AlphaCurve = AnimationCurve.Linear(0, 1, 1, 1);
+
+        protected override void HandleShow()
+        {
+            base.HandleShow();
+            m_AnimGroup.alpha = 0f;
+        }
+
+        protected override void HandleAnimation(float ratio)
+        {
+            // set alpha
+            float alphaRatio = m_AlphaCurve.Evaluate(ratio);
+            m_AnimGroup.alpha = alphaRatio;
+        }
+    }
+}
