0aaadda23f4956ff37aa6225df3539cffc27965d
Luke <luke@spierek.net>
2019-05-14 19:21:39 +0200

    Updated UIMoveAnimWidget

--- Code/UI/Animation/UIMoveAnimWidget.cs
+++ Code/UI/Animation/UIMoveAnimWidget.cs
@@ -12,31 +12,17 @@ namespace LSTools
 
         [Header("Horizontal")]
         [SerializeField]
-        private Vector2 m_HorizontalStartPos = new Vector2(0.7f, 0.4f);
+        private Vector2 m_StartPos = new Vector2(0.7f, 0.4f);
         [SerializeField]
-        private Vector2 m_HorizontalEndPos = new Vector2(0.9f, 0.7f);
-
-        [Header("Vertical")]
-        [SerializeField]
-        private Vector2 m_VerticalStartPos = new Vector2(0.7f, 0.4f);
-        [SerializeField]
-        private Vector2 m_VerticalEndPos = new Vector2(0.9f, 0.7f);
+        private Vector2 m_EndPos = new Vector2(0.9f, 0.7f);
 
         protected override void HandleAnimation(float ratio)
         {
             // set anchors and adjust position
             float posRatio = m_MovementCurve.Evaluate(ratio);
             Vector2 targetPos = Vector2.zero;
-            if (GolfManager.Instance.IsHorizontal)
-            {
-                targetPos.x = Mathf.Lerp(m_HorizontalStartPos.x, m_HorizontalEndPos.x, posRatio);
-                targetPos.y = Mathf.Lerp(m_HorizontalStartPos.y, m_HorizontalEndPos.y, posRatio);
-            }
-            else
-            {
-                targetPos.x = Mathf.Lerp(m_VerticalStartPos.x, m_VerticalEndPos.x, posRatio);
-                targetPos.y = Mathf.Lerp(m_VerticalStartPos.y, m_VerticalEndPos.y, posRatio);
-            }
+            targetPos.x = Mathf.Lerp(m_StartPos.x, m_EndPos.x, posRatio);
+            targetPos.y = Mathf.Lerp(m_StartPos.y, m_EndPos.y, posRatio);
 
             m_AnimTransform.anchorMin = targetPos;
             m_AnimTransform.anchorMax = targetPos;
